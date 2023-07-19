50027dcfcee7141c70102b27b2ea0b1cd1f3e4d1
Vyshnavi17-11 <135938623+Vyshnavi17-11@users.noreply.github.com>
2023-07-19 10:41:49 +0530

    Add files via upload

---
+++ srcipt.js
@@ -0,0 +1,11 @@
+function acon(){
+    document.getElementById("AC").src="https://cdn.pixabay.com/photo/2021/09/08/07/20/air-conditioner-6605973_640.jpg";
+}
+function acoff(){
+    document.getElementById("AC").src="https://www.mrright.in/ideas/wp-content/uploads/2015/02/Picture6-802x666.png";
+}
+function actemperature(){
+    document.getElementById("AC").src="https://www.canstarblue.com.au/wp-content/uploads/2016/12/new-air-conditioner-remote-temperature.jpg";
+
+}
+
